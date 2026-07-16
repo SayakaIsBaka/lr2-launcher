a88c610937c757e4e3243b82187b7dd9e8696491
Sayaka <alice@lolita.technology>
2026-07-16 18:59:37 +0200

    Removed static and replaced with local vec in directsound.rs

--- src/directsound.rs
+++ src/directsound.rs
@@ -1,24 +1,26 @@
+use std::ffi::c_void;
 use slint::SharedString;
 use anyhow::Result;
 use windows::{Win32::Media::Audio::DirectSound::{DSDEVID_DefaultPlayback, DirectSoundEnumerateA}, core::{BOOL, GUID, PCSTR}};
 
-static DEVICES: std::sync::RwLock<Vec<SharedString>> = std::sync::RwLock::new(vec![]);
-
-unsafe extern "system" fn ds_enum_callback(guid: *mut GUID, lpcstr_description: PCSTR, _: PCSTR, _: *mut std::ffi::c_void) -> BOOL {
+unsafe extern "system" fn ds_enum_callback(guid: *mut GUID, lpcstr_description: PCSTR, _: PCSTR, user_ptr: *mut std::ffi::c_void) -> BOOL {
     // Don't add the meta "Default playback" device (LR2 doesn't show it)
     if !guid.is_null() && unsafe { *guid } != DSDEVID_DefaultPlayback {
-        let mut devices = DEVICES.write().unwrap();
+        let ptr2 = user_ptr as *mut Vec<SharedString>;
+        let devices = unsafe { &mut *ptr2 };
         devices.push(unsafe { lpcstr_description.to_string().unwrap().into() });
     };
     return BOOL(1);
 }
 
 pub fn get_devices() -> Result<Vec<SharedString>> {
-    DEVICES.write().unwrap().clear();
+    let mut devices: Vec<SharedString> = vec![];
+    let myptr: *mut Vec<SharedString> = &mut devices;
+    let voidptr = myptr as *mut c_void;
 
     unsafe {
-        DirectSoundEnumerateA(Some(ds_enum_callback), None)?;
+        DirectSoundEnumerateA(Some(ds_enum_callback), Some(voidptr))?;
     };
 
-    Ok(DEVICES.read().unwrap().to_vec())
+    Ok(devices)
 }
